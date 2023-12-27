7e7820af067bc93991acdfbe7cade5fed4072ce6
Jared Lewis <jlewis7@uh.edu>
2023-12-27 00:04:23 -0600

    Updates to guessing.py

--- Guessing.py
+++ Guessing.py
@@ -13,25 +13,4 @@ def guess(x):
         else:
             print(f"Great job, you found the number {random_number} correctly!!")
 
-guess(10)
-
-# Guess the number (user has computer guess)
-import random
-def computer_guess(x):
-    low = 1
-    high = x
-    feedback = ""
-    while feedback != "c":
-        if low != high:
-            guess2 = random.randint(low, high)
-        else:
-            guess = low                         # doesn't matter could be high
-        feedback = input(f"Is {guess2} too high (H), too low (L), or very correct (C)?").lower()
-        if feedback == 'h':
-            high = guess2 - 1
-        elif feedback == 'l':
-            low = guess2 + 1
-
-    print(f'Yay, Great job Computer! You correctly guessed {guess2}')
-
-computer_guess(25)
+guess(34)
